eca23aa6440af9af9de08775fb700e59c4e8aec9
Kévin Martin <hello@kmartin.io>
2021-07-17 22:21:41 +0200

    bugfix #2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @if (Route::currentRouteName() == 'home')
-        <title>{{ env('APP_NAME')}} - @yield('title')</title>
+        <title>{{ config('app.name') }} - @yield('title')</title>
     @else
-        <title>@yield('title') - {{ env('APP_NAME')}}</title>
+        <title>@yield('title') - {{ config('app.name') }}</title>
     @endif
     
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
@@ -26,14 +26,14 @@
 <body class="flex flex-col h-screen bg-gray-800">
     <div class="flex flex-col w-full h-full container mx-auto">
         <header class="flex flex-col md:flex-row items-center content-beetwen pt-6 mx-8 mb-auto">
-            <a class="logo flex text-4xl font-extrabold text-white mb-3 md:mb-0" href="{{ url('/') }}">{{ env('APP_NAME') }}</a>
+            <a class="logo flex text-4xl font-extrabold text-white mb-3 md:mb-0" href="{{ url('/') }}">{{ config('app.name') }}</a>
             @include('layouts.menus.main')
         </header>
         @yield('content')
         <footer class="flex flex-col md:flex-row-reverse items-center content-beetwen mx-8 mt-auto pb-6 text-white">
             @include('layouts.menus.footer')
             <div class="mb-2 md:mb-0 mx-auto">Made from 🇫🇷</div>
-            <div>Copyright 2020 - {{ env('APP_NAME') }}</div>
+            <div>Copyright 2020 - {{ config('app.name') }}</div>
         </footer>
     </div>
     <script src="js/jquery.min.js"></script>

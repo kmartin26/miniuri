3bc51f121766d87ba20988047275a87e6c8164d3
Kévin Martin <hello@kmartin.io>
2021-08-22 16:01:13 +0200

    added restart button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <h2 class="text-white text-4xl font-bold">Past and shorten</h2>
     <form action="{{ route('home') }}" class="form flex items-center justify-center mx-8 xl:mx-0 pt-2">
         <input type="text" name="url" class="h-10 w-full sm:w-1/2 xl:w-1/3 outline-none rounded-none rounded-l-lg p-2">
-        <button type="submit" class="btn btn-submit flex justify-center font-bold py-2 px-4 outline-none rounded-none rounded-r-lg">
+        <button type="submit" class="btn btn-submit flex justify-center items-center font-bold py-2 px-4 outline-none rounded-none rounded-r-lg">
             <span class="mr-1">Go</span>
             <svg xmlns="http://www.w3.org/2000/svg" class="ico-flash" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
@@ -17,8 +17,16 @@
             </svg>
         </button>
     </form>
-    <div class="alert-box w-1/2 h-12 mx-auto mt-3" role="alert">
+    <div class="alert-box w-1/2 h-12 mx-auto my-3" role="alert">
         <div class="alert hidden"></div>
+    </div>
+    <div class="restart flex items-center justify-center h-10">
+        <button class="btn btn-restart flex justify-center items-center font-bold py-2 px-4 outline-none rounded-lg hidden">
+            <span class="mr-1">Restart</span> 
+            <svg xmlns="http://www.w3.org/2000/svg" class="ico-restart" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+            </svg>
+        </button>
     </div>
 </section>
 @endsection
